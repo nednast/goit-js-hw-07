c39b2953104e77713f828165542aca460e0ce3ca
NNASTYA2007 <nedyaknast@gmail.com>
2023-12-26 13:06:22 +0100

    Update task-1.js

---
+++ js/task-1.js
@@ -0,0 +1,11 @@
+const list = document.querySelector("#categories");
+const listItems = list.querySelectorAll(".item");
+console.log(`Number of categories: ${listItems.length}`);
+
+listItems.forEach((el) => {
+  let title = el.querySelector("h2").textContent;
+  let content = el.querySelectorAll("li").lenght;
+
+  console.log(`Category: ${title}`);
+  console.log(`Elements: ${content}`);
+});
