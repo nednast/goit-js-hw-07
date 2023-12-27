fbc3ccea33fece811dcfa4865dacdbf58aa860ef
NNASTYA2007 <nedyaknast@gmail.com>
2023-12-27 15:25:51 +0100

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -2,10 +2,7 @@ const input = document.querySelector("#name-input");
 const title = document.querySelector("#name-output");
 
 input.addEventListener("input", (event) => {
-  if (
-    event.currentTarget.value === "" ||
-    event.currentTarget.value.trim() === ""
-  ) {
+  if (event.currentTarget.value.trim() === "") {
     title.textContent = "Anonymous";
   } else {
     title.textContent = event.currentTarget.value.trim();
