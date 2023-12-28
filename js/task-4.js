06be6c042c2073c0a88ef644d8f9751c3a2431dc
NNASTYA2007 <nedyaknast@gmail.com>
2023-12-28 17:33:59 +0100

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -15,8 +15,8 @@ const form = document.querySelector(".login-form");
 form.addEventListener("submit", handlerSubm);
 function handlerSubm(event) {
   event.preventDefault();
-  const emailTarg = form.elements.email.value;
-  const passwordTarg = form.elements.password.value;
+  const emailTarg = form.elements.email.value.trim();
+  const passwordTarg = form.elements.password.value.trim();
   if (emailTarg === "" || passwordTarg === "") {
     alert("All form fields must be filled in");
   } else {
